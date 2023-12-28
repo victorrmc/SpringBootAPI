a323931a8cfbccdbd89b3ab2d672d297c0fffea7
Victor Marrero <victor.mcarrillo@bitboxonline.com>
2023-12-28 10:33:36 +0000

    Terminado login con Spring Security

--- src/main/java/com/bitboxer2/SpringBootAPI/JWT/JwtService.java
+++ src/main/java/com/bitboxer2/SpringBootAPI/JWT/JwtService.java
@@ -1,6 +1,6 @@
 package com.bitboxer2.SpringBootAPI.JWT;
 
-import com.bitboxer2.SpringBootAPI.User.User;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
@@ -12,13 +12,13 @@ import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
-
+import java.util.function.Function;
 
 
 @Service
 public class JwtService {
-    private static final String SECRET_KEY="t9oBj1m9EEk4dM3BoRKILFv8OgbQGvu2ePzazBDFK2qOfpERLJZEyagoL8FC";
+    private static final String SECRET_KEY = "t9oBj1m9EEk4dM3BoRKILFv8OgbQGvu2ePzazBDFK2qOfpERLJZEyagoL8FC";
+
     public String getToken(UserDetails user) {
         return getToken(new HashMap<>(), user);
     }
@@ -29,7 +29,7 @@ public class JwtService {
                 .setClaims(extraClaims)
                 .setSubject(user.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis()+1000*60*24))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
                 .signWith(getKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
@@ -37,5 +37,36 @@ public class JwtService {
     private SecretKey getKey() {
         byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String getUsernameFromToken(String token) {
+        return getClaim(token, Claims::getSubject);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = getUsernameFromToken(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private Claims getAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public <T> T getClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Date getExpiration(String token) {
+        return getClaim(token, Claims::getExpiration);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return getExpiration(token).before(new Date());
     }
 }
